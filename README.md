36772016fdc1bb1728c7c7ed46e18deea00a1ce3
yasha <wade333@list.ru>
2022-08-10 19:43:10 +0300

    1rev

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# praktikum_new_diplom
+![workflow](https://github.com/garmagelm/foodgram-project-react/actions/workflows/foodgram_project.yml/badge.svg)
+# Проект: Foodgram
+Приложение для публикации, редактирования и добавления в избранное рецептов с функцией "корзина покупок", которая предоставляет список покупок для всех выбранных рецептов.
+
+Работает на базе Django & React.
+
+***Установка:***
+1. Устанавливаем [Docker](https://docs.docker.com/engine/install/).
+2. Клонируем репозитарий из GitHub командой в терминале 
+```bash
+git clone git@github.com:Thx-Phila/foodgram-project-react.git
+```
+
+***Настройка переменного окружения .env:***
+
+* Создаем виртуальное окружение командой 
+```bash
+python3 -m venv venv
+```
+* Создаем файл .env
+* Вводим значения секретов: 
+```
+DB_ENGINE=django.db.backends.postgresql
+DB_NAME=<название вашей базы данных>
+POSTGRES_USER=<имя пользователя в postgres>
+POSTGRES_PASSWORD=<пароль пользователя>
+DB_HOST=db  # Сюда можете прописать localhost, либо оставить, если будете использовать docker-compose
+DB_PORT=5432
+```
+
+***Команды для Docker***
+1. Запускаем контейнер из папки infra командой
+```bash
+docker-compose up --build -d
+```
+2. Выполняем миграцию командой 
+```bash
+docker-compose exec backend python manage.py migrate --noinput
+```
+3. Создаем суперпользователя 
+```bash
+docker-compose exec backend python manage.py createsuperuser
+```
+4. Собераем файлы статики в одну директорию: 
+```bash
+docker-compose exec backend python manage.py collectstatic --no-input
+```
+5. В backend создаем фикстуры 
+```bash
+docker-compose exec backend python manage.py loaddata ingredients.json
+```
+6. Команда для остановки запущенных docker-контейнеров и удаление их:
+```bash
+docker-compose down
+```
+
+***Стек технологий:***
+
+* Python3
+* Django
+* Django REST Framework
+* Docker Desktop
+* Posgresql
+
+***Автор проекта:***
+Ворончихин Яков
---
+++ backend/foodgram/settings.py
@@ -0,0 +1,119 @@
+import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = os.getenv("SECRET_KEY", default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs)')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'djoser',
+    'api.apps.ApiConfig',
+    'recipes.apps.RecipesConfig',
+    'users.apps.UsersConfig',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'foodgram.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'foodgram.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+}
+
+AUTH_USER_MODEL = 'users.User'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.FoodPagination',
+    'PAGE_SIZE': 9,
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
