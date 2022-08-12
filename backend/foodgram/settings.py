e196bb798a1d071b7160d2073da70a80c6900791
yasha <wade333@list.ru>
2022-08-12 19:26:33 +0300

    q

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs)')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.155.201', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['84.201.155.201']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
