40afee8a6e90f371a22382630ef524bebf78b3ca
yasha <wade333@list.ru>
2022-08-13 12:51:30 +0300

    as

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs)')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['51.250.18.26',
                  'localhost',
