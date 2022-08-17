7fffca39544c69e59be3478443d1c6c17ab96e87
yasha <wade333@list.ru>
2022-08-17 20:17:24 +0300

    as

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["51.250.18.26",]
+ALLOWED_HOSTS = ["51.250.18.26"]
 
 
 INSTALLED_APPS = [
