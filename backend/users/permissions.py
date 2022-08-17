2640e1d80b900d20fdf02680b9c844a6f9c8e00f
yasha <wade333@list.ru>
2022-08-17 20:43:12 +0300

    as

--- backend/users/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions
-
-
-class IsAuthenticatedAndReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                and request.user.is_authenticated)
